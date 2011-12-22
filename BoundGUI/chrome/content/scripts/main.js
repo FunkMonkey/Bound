f79a8a0eed476176117af56f844ae390db4602db
André <andre.selmanagic+ququ@gmail.com>
2011-12-22 21:45:32 +0100

    xul tree with cpp data

--- BoundGUI/chrome/content/scripts/main.js
+++ BoundGUI/chrome/content/scripts/main.js
@@ -5,18 +5,41 @@ const Cu = Components.utils;
 Cu.import("chrome://bound/content/modules/log.jsm");
 Cu.import("chrome://bound/content/modules/Bound.jsm");
 Cu.import("chrome://bound/content/modules/CPPAnalyzer.jsm");
+Cu.import("chrome://bound/content/modules/TreeData.jsm");
 
 window.addEventListener("close", Bound.quit, false); 
 
 
 CPPAnalyzer.init();
 
-var json = null;
+var cppAST = null;
+
+function getCellTextCB(item, row, col)
+{
+	if(!item){
+		return "error";
+	}
+	
+	if(item.data.name != "")
+		return item.data.kind + " " + item.data.name;
+	else
+		return "<anonymous>";
+}
 
 function testParsing()
 {
-	json = CPPAnalyzer.parse_header(["supertest", "D:\\Data\\Projekte\\Bound\\src\\CPPAnalyzer\\Test\\test1.cpp"]);
+	cppAST = CPPAnalyzer.parse_header(["supertest", "D:\\Data\\Projekte\\Bound\\src\\CPPAnalyzer\\Test\\test1.cpp"]);
+	let tree = document.getElementById("cppTree");
+	
+	log(" " + tree);
+	
+	let treeView = new nsTreeView(cppAST.root.children, getCellTextCB);
+	
+	tree.view = treeView;
 }
+
+window.addEventListener("load", testParsing, true);
+
 
 
 
