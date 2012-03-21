f8ef81d788562c6ac2eb3e122357ecb004f4de1d
André <andre.selmanagic+ququ@gmail.com>
2012-03-21 18:39:57 +0100

    Thesis * wrapping: error handling, wrapping instances, generic wrapper

--- Wrapping/Spidermonkey/WrappingTest/src/main.cpp
+++ Wrapping/Spidermonkey/WrappingTest/src/main.cpp
@@ -36,8 +36,33 @@ void reportError(JSContext *cx, const char *message, JSErrorReport *report)
             message);
 }
 
+class any_ptr {
+
+	void* ptr_;
+	void (*thr_)(void*);
+
+	template <typename T>
+	static void thrower(void* ptr) { throw static_cast<T*>(ptr); }
+
+public:
+
+	template <typename T>
+	any_ptr(T* ptr) : ptr_(ptr), thr_(&thrower<T>) {}
+
+	template <typename U>
+	U* cast() const {
+		try { thr_(ptr_); }
+		catch (U* ptr) { return static_cast<U*>(ptr); }
+		catch (...) {}
+		return 0;
+	}
+};
+
 std::string loadStringFromFile(const std::string& filename)
 {
+	int* iPtr = new int(3);
+	any_ptr anyPtr(iPtr);
+
 	std::ifstream inputStream(filename.c_str(), std::ifstream::in);
 
 	if(!inputStream)
